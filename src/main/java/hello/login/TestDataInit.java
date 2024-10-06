a87429a026a7584bb7a67b8d1dfed454e43fda32
Do-oya <qkrwjdgus1441@naver.com>
2024-10-06 19:44:33 +0900

    [김영한 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술]

---
+++ src/main/java/hello/login/TestDataInit.java
@@ -0,0 +1,31 @@
+package hello.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스트");
+
+        memberRepository.save(member);
+    }
+
+}
